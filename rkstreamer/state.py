41856cbc1d6e3f917d74fdbc8d777f12f0306ea3
Ray A <arp.rayappan@gmail.com>
2023-05-28 06:34:40 +0530

    exc handling

--- rkstreamer/state.py
+++ rkstreamer/state.py
@@ -1,5 +1,6 @@
 """RKStreamer - State and State machine module"""
 
+from rkstreamer.models.exceptions import InvalidInput, QueueException
 
 class State:
     """State class"""
@@ -35,10 +36,13 @@ class StateMachine:
     def trigger(self):
         """Triggers the state change or handle current state"""
         while True:
-            user_input = input(self.current_state.prompt)
+            try:
+                user_input = input(self.current_state.prompt)
+            except KeyboardInterrupt:
+                raise SystemExit("\n\nTata! See you soon!!") from None
             if user_input.lower().startswith('-e'):
                 raise SystemExit('Tata!')
-            elif user_input.startswith("--"):
+            if user_input.startswith("--"):
                 state_name = user_input[2:]
                 if (state_name != self.current_state.name) and (state_name in self.states):
                     # stop player when switching mode.
@@ -46,4 +50,9 @@ class StateMachine:
                     self.current_state = self.states[state_name]
             else:
                 if user_input:
-                    self.current_state.handle_input(user_input)
+                    try:
+                        self.current_state.handle_input(user_input)
+                    except InvalidInput as exc:
+                        print(f"Error: {exc.__class__.__name__}, Desc: 'Invalid Input'")
+                    except QueueException as exc:
+                        print(f"Error: {exc.__class__.__name__}, Desc: 'Invalid Queue operation'")
